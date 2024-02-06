0a798e5346f27b880873a26f4591cd8fe9c820a6
Nightshadow1258 <paul.reisch43@gmail.com>
2024-02-06 22:06:29 +0100

    finished greyscale, sepia and refelct filter for the filter-less exercise

--- ProblemSet4/filter-less/helpers.c
+++ ProblemSet4/filter-less/helpers.c
@@ -1,25 +1,128 @@
 #include "helpers.h"
 
+BYTE round_byte(float num)
+{
+    int temp = (num < 0 ? (num - 0.5) : (num + 0.5)); // round manually
+    // ensure values stay in range of 8 bits.
+    if (temp > 255)
+        temp = 255;
+    if (temp < 0)
+        temp = 0;
+
+    return (BYTE) temp;
+}
+
+void swap_pixels(RGBTRIPLE* x, RGBTRIPLE* y)
+{
+    RGBTRIPLE temp = *x;
+    *x = *y;
+    *y = temp;
+    return;
+}
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // NTSC formula: 0.299 ∙ Red + 0.587 ∙ Green + 0.114 ∙ Blue 
+    // source: https://support.ptc.com/help/mathcad/r9.0/en/index.html#page/PTC_Mathcad_Help/example_grayscale_and_color_in_images.html
+    // TODO iterate over the entire image and convert the colors to grayscale.
+    // not sure if there should be steps for the greyscale or just balck or white -> will go for multiple steps.
+    // for black white we would need a metric to deceide when a pixel is considered white or black 
+    // Different ideas to create a greyscale filter
+    // average of the rgb values
+    // (min(r,g,b) + max(r,g,b))/2
+    // ntsc formula from above
+    // than set rgb all to the same value.
+
+    for(int h = 0; h < height; h++)
+    {
+        for( int w = 0; w < width; w++)
+        {
+            BYTE blue = image[h][w].rgbtBlue;
+            BYTE green = image[h][w].rgbtGreen;
+            BYTE red = image[h][w].rgbtRed;
+            float grey; // ennsure that there is no overflow WORD is 16 bit that is more than enough 10 bit needed only
+            // not sure what the best way is to avoid this issue
+
+            // average methode:
+            grey = round_byte( (blue + green + red) /3.0 ); 
+
+            // ntsc:
+            grey =round_byte( 0.299 * red + 0.587 * green + 0.114 * blue);
+
+    
+            // overwrite date in original image buffer don't forget to cast back to BYTE
+            image[h][w].rgbtBlue = (BYTE) grey;
+            image[h][w].rgbtGreen = (BYTE) grey;
+            image[h][w].rgbtRed = (BYTE) grey;
+        }
+    }
+    // tested and looking good. both average and ntsc look fine to me.
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // research on sepia: image appears more yellow and washed out.. Originally due to UV light expose over longer time.
+    // conversion from wikipedia: https://de.wikipedia.org/wiki/Sepia_(Fotografie)
+    // R = R * 0.393 + G * 0.769 + B * 0.189
+    // G = R * 0.349 + G * 0.686 + B * 0.168
+    // B = R * 0.272 + G * 0.534 + B * 0.131
+    // pretty much copy and paste from greyscale only the filtering is differnt.
+    
+    for(int h = 0; h < height; h++)
+    {
+        for( int w = 0; w < width; w++)
+        {
+            int blue = image[h][w].rgbtBlue;
+            int green = image[h][w].rgbtGreen;
+            int red = image[h][w].rgbtRed;
+            float blue_sepia,green_sepia,red_sepia; // ennsure that there is no overflow WORD is 16 bit that is more than enough 10 bit needed only
+            // not sure what the best way is to avoid this issue
+            
+            red_sepia = round_byte(red * 0.393 + green * 0.769 + blue * 0.189);
+            green_sepia = round_byte(red * 0.349 + green * 0.686 + blue * 0.168);
+            blue_sepia = round_byte(red * 0.272 + green * 0.534 + blue * 0.131);
+
+            // overwrite date in original image buffer don't forget to cast back to BYTE
+            image[h][w].rgbtBlue = (BYTE) blue_sepia;
+            image[h][w].rgbtGreen = (BYTE) green_sepia;
+            image[h][w].rgbtRed = (BYTE) red_sepia;
+        }
+    }
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // reflect horizontally. I think that means from top to bottom? so it is mirrored along the horizontal axis.
+    // so we do not need to calculate new values for rgb and only need to rearrange the pixels of the image.
+    // Idea iterate over the top half of the image and swap the pixels with each other
+    // use pointers for memory efficency 
+    // I believe that the start point 0,0 is in the left top corner by convention,, if not bad for me :)
+
+
+    for(int h = 0; h <  height/2; h++) // I believe h is always dividable by two and four? maybe make sure.
+    {
+        for( int w = 0; w < width; w++)
+        {
+            swap_pixels(&image[h][w],&image[height-h-1][w]);
+            // RGBTRIPLE temp = image[h][w];
+            // image[h][w] = image[height-h-1][w];
+            // image[height-h-1][w] = temp;
+        }
+    }
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    
+    
     return;
 }
