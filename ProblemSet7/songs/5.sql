04fb69334f6bfb356fa642732336b12d577848a0
Nightshadow1258 <paul.reisch43@gmail.com>
2024-02-22 22:48:06 +0100

    Fnished songs exercise.

---
+++ ProblemSet7/songs/5.sql
@@ -0,0 +1,1 @@
+SELECT ROUND(AVG(energy),3) FROM songs;
---
+++ ProblemSet7/songs/7.sql
@@ -0,0 +1,3 @@
+SELECT AVG(energy) FROM songs WHERE artist_id=(SELECT id FROM artists WHERE name='Drake')
+;SELECT AVG(energy) FROM songs JOIN artists ON artists.id = songs.artist_id WHERE artists.name='Drake';
+
